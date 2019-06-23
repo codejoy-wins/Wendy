568a8d41b37f21823edaf449ee98a3980d526092
Max <mjann@ucsc.edu>
2019-06-23 16:43:21 -0700

    wendy jann

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import wendy from './wendy.png';
+import wendy2 from './wendy2.jpeg';
+import wendy3 from './wendy3.jpeg';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <div>
+          <h1>Wendy Jann</h1>
+        </div>
+        <div>
+          <img src={wendy2}></img>
+        </div>
+        <div>
+          <span>(805) 660 9376</span>
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={wendy} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I <code>am on</code> the left!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://maxjann.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jann Software
         </a>
       </header>
     </div>
